6a4bf81d83ec903614e920086196297126832bcd
Daihan <daihan@revesoft.com>
2023-05-11 10:25:27 +0600

    section 2 - 4 UI setup

---
+++ Udemy-push-notification-course/Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Udemy-push-notification-course
+//
+//  Created by REVE Systems on 11/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func timerTapped(_ sender: Any) {
+        print("timer")
+    }
+    
+    @IBAction func dateTapped(_ sender: Any) {
+        print("date")
+    }
+    
+    @IBAction func onLocationTapped(_ sender: Any) {
+        print("location")
+    }
+    
+}
+
