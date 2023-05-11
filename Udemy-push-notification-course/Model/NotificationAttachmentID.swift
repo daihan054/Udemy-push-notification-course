8e80bb562825d3ed8fac00152acd5526ca840b0e
Daihan <daihan@revesoft.com>
2023-05-11 12:46:12 +0600

    Section 2: Local Notifications on iOS - Attachments

---
+++ Udemy-push-notification-course/Model/NotificationAttachmentID.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationAttachmentID.swift
+//  Udemy-push-notification-course
+//
+//  Created by REVE Systems on 11/5/23.
+//
+
+import Foundation
+
+enum NotificationAttachmentID: String {
+    case timer = "userNotification.attachment.timer"
+    case date = "userNotification.attachment.date"
+    case location = "userNotification.attachment.location"
+}
